ca4fc5dcc24bb53d1e0bc4d6177ccbf928eb3e6c
BalintB4 <balazsbalint513@gmail.com>
2025-01-09 23:00:19 +0100

    update

--- .github/cicd/continous-testing-demo-scripts/run-integration-test.sh
+++ .github/cicd/continous-testing-demo-scripts/run-integration-test.sh
@@ -3,15 +3,15 @@ set -e
 
 echo "Current working directory: $(pwd)"
 
-# 1. Finde das Root-Verzeichnis des Projekts
+
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 echo "Project root directory: ${PROJECT_ROOT}"
 
-# 2. Navigiere zum devservices-demo Modul
+
 cd "${PROJECT_ROOT}/continuous-testing-demo"
 
-# 3. Führe die Tests aus
+
 echo "Running tests in: $(pwd)"
-./mvnw test -Dtest="at.htl.leonding.boundary.**.*Test" # Für boundary-Tests
+./mvnw test -Dtest="at.htl.leonding.boundary.**.*Test"
 
 echo "Test execution finished in: $(pwd)"
--- .github/cicd/continous-testing-demo-scripts/run-unit-test.sh
+++ .github/cicd/continous-testing-demo-scripts/run-unit-test.sh
@@ -3,14 +3,14 @@ set -e
 
 echo "Current working directory: $(pwd)"
 
-# 1. Finde das Root-Verzeichnis des Projekts
+
 PROJECT_ROOT=$(git rev-parse --show-toplevel)
 echo "Project root directory: ${PROJECT_ROOT}"
 
-# 2. Navigiere zum devservices-demo Modul
+
 cd "${PROJECT_ROOT}/continuous-testing-demo"
 
-# 3. Führe die Tests aus
+
 echo "Running tests in: $(pwd)"
 ./mvnw test -Dtest="at.htl.leonding.entity.**.*Test"
 
